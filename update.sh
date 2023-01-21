b2c3da27c8c9e78f75e13a03f64e86bc3d06ed1d
noradogg <ndktr.n9h93@gmail.com>
2023-01-21 15:30:18 +0900

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+# git pullして、ローカルのプログラムを、リモートリポジトリのプログラムに更新する
+function gt_pull() {
+    git pull origin main
+}
+
+mds="masterdatastore"
+(cd $mds && gt_pull)
+adm="admin"
+(cd $adm && gt_pull)
+web="web"
+(cd $web && gt_pull)
